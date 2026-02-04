3ecf1e21fe3df57a3d4629fa4bded93e584afa06
1RN18CS013 <95519087+1RN18CS013@users.noreply.github.com>
2026-02-04 11:07:58 -0800

    Constructors with various calling methods
    Constructors with various calling methods with main class and constructor class

---
+++ Constructors.java
@@ -0,0 +1,18 @@
+public class Constructors
+{
+    //no params no return value-m1
+void m1(){
+    System.out.println("hello");
+}
+
+String m2()
+       {
+          return("Helllllloooooo");
+       }
+void m3(String name){
+    System.out.println("Hello "+name);
+}
+    String m4(String kname){
+        return("Hello "+kname);
+    }
+}
---
+++ Constructorsmainclass.java
@@ -0,0 +1,12 @@
+public class Constructorsmainclass {
+    public static void main(String[] args){
+        Constructors c= new Constructors();
+        c.m1();
+        String s=c.m2();
+        System.out.println(s);
+        c.m3("Ananya");
+        String e=c.m4("Ikshvaku");
+        System.out.println(e);
+
+    }
+}
